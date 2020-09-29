96f1e364dd6438cb85a469f8a0fdb93b0357adcd
w403202153 <403202153@qq.com>
2020-09-29 20:37:51 +0800

    run command print 1234

--- spring-boot-netty/study-netty/study-client/src/main/java/org/yyx/netty/study/StudyClientApplication.java
+++ spring-boot-netty/study-netty/study-client/src/main/java/org/yyx/netty/study/StudyClientApplication.java
@@ -13,6 +13,6 @@ public class StudyClientApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.println("1234");
     }
 }
